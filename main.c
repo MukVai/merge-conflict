12428792108dc18398a6c0d36dd7c3c5696880d9
MukVai <mukeshvaishnav018@gmail.com>
2023-03-15 16:41:42 +0530

    Updated Feature 1

--- main.c
+++ main.c
@@ -4,20 +4,17 @@ int feature() {
   return 1;
 }
 
+int feature1() {
+	return 2;
+}
+
 int security() {
   return -1;
 }
 
 int main() {
-  int t = feature() + security();
+  int t = feature() + security() + feature1();
   printf("%d", t);
   
   return 0;
 }
-
-feature1()
-{
-  //Sample Code for Feature 1
-}
-
-
